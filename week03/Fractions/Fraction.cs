204003d57aacb4b3dbda2ef23864ae7031d91da4
sharedlopezserrano <slopezserrano@byupathway.edu>
2025-05-21 20:33:33 -0500

    Week 03 Fraction

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,43 @@
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int value)
+    {
+        _top = value;
+    }
+    
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / _bottom;
+    }
+}
