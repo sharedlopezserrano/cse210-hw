53671e89d3c5fb2cd4bb252f6cb9885741c52000
sharedlopezserrano <slopezserrano@byupathway.edu>
2025-06-09 15:32:43 -0300

    Homework week 04

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment a1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+
+        MathAssignment m1 = new MathAssignment("Roberto Rodriguez", "Fractions", "Section 7.3", "8-19");
+        Console.WriteLine(m1.GetSummary());
+        Console.WriteLine(m1.GetHomeworkList());
+
+        WritingAssignment w1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(w1.GetSummary());
+        Console.WriteLine(w1.GetWritingInformation());
     }
 }
