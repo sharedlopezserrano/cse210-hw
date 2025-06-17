8e9865b4e201dc2b7dcde169759063bf84d3c6a0
SharedLopez <shared@socrates.la>
2025-06-17 11:58:13 -0300

    week 06 commit Shape.cs

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Rectangle("Red", 5, 10));
+        shapes.Add(new Rectangle("Blue", 7, 7));
+        shapes.Add(new Circle("Green", 4));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea():0.00}");
+        }
     }
 }
